e40f17f95500200ea08e63b3e060b8652fdbb7db
Arthur Martins <showzin048@gmail.com>
2024-09-03 11:13:27 -0300

    Fiz a modelagem da classe entity de usuario e mensagem

--- src/main/java/com/tarefa/opombo/model/entity/Mensagem.java
+++ src/main/java/com/tarefa/opombo/model/entity/Mensagem.java
@@ -2,6 +2,10 @@ package com.tarefa.opombo.model.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Table
@@ -11,4 +15,21 @@ public class Mensagem {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    private String texto;
+
+    private LocalDateTime dataHoraCriacao;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @ManyToMany
+    private Set<Usuario> like;
+
+    @ManyToMany
+    @JoinTable(name = "mensagem_curtida", joinColumns = @JoinColumn(name = "id_usuario"), inverseJoinColumns = @JoinColumn(name = "id_mensagem"))
+    private Set<Mensagem> curtidas;
+
+
 }
--- src/main/java/com/tarefa/opombo/model/entity/Usuario.java
+++ src/main/java/com/tarefa/opombo/model/entity/Usuario.java
@@ -1,11 +1,14 @@
 package com.tarefa.opombo.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.tarefa.opombo.model.enums.PerfilAcesso;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.Data;
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.validator.constraints.br.CPF;
+
+import java.util.Set;
 
 @Entity
 @Table
@@ -34,4 +37,10 @@ public class Usuario {
     @CPF
     @Column(nullable = false)
     private String cpf;
+
+    @OneToMany(mappedBy = "usuario")
+    private Set<Mensagem> mensagens;
+
+    @ManyToMany(mappedBy = "curtidas")
+    private Set<Mensagem> mensagensCurtidas;
 }
